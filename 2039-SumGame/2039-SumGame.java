ee1f6de18f1a5800f838714bf4ac07bda6a7cf43
Kavya Sree S S <kavyasreesundar9@gmail.com>
2026-07-09 09:11:14 +0530

    Create 2039-SumGame

---
+++ 2039-SumGame/2039-SumGame.java
@@ -0,0 +1,10 @@
+// Last updated: 09/07/2026, 09:11:14
+class Solution {
+    public boolean sumGame(String num) {
+         int n = num.length();
+        double res = 0;
+        for (int i = 0; i < n; ++i)
+            res += (i < n / 2 ? 1 : -1) * (num.charAt(i) == '?' ? 4.5 : num.charAt(i) - '0');
+        return res != 0;
+    }
+}
