cc30fd75c4bcc5bdce87ba86c05aacb0378af216
Kavya Sree S S <kavyasreesundar9@gmail.com>
2026-07-09 09:11:23 +0530

    Create 1636-NumberOfSubstringsWithOnly1s

---
+++ 1636-NumberOfSubstringsWithOnly1s/1636-NumberOfSubstringsWithOnly1s.java
@@ -0,0 +1,18 @@
+// Last updated: 09/07/2026, 09:11:23
+class Solution {
+    public int numSub(String s) {
+        long ans = 0;
+        long count = 0;
+        int mod = 1000000007;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                count++;
+                ans = (ans + count) % mod;
+            } else {
+                count = 0;
+            }
+
+        }
+        return (int) ans;
+    }
+}
