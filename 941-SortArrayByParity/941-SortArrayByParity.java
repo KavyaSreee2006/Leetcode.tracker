65dee1fcfe8be61a4a5a5234b2af6a006c6d4d13
Kavya Sree S S <kavyasreesundar9@gmail.com>
2026-07-09 09:11:40 +0530

    Create 941-SortArrayByParity

---
+++ 941-SortArrayByParity/941-SortArrayByParity.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:11:40
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int[] result = new int[nums.length];
+        int evenIndex = 0;
+        int oddIndex = nums.length - 1;
+
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                result[evenIndex] = num;
+                evenIndex++;
+            } else {
+                result[oddIndex] = num;
+                oddIndex--;
+            }
+        }
+
+        return result;
+    }
+}
