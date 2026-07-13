f88cedec51d9ce0fd2b4d83b4a496004992a7f5c
Kavya Sree S S <kavyasreesundar9@gmail.com>
2026-07-13 11:22:18 +0530

    File updated at 13/07/2026, 11:22:17

--- 2544-AlternatingDigitSum/2544-AlternatingDigitSum.java
+++ 2544-AlternatingDigitSum/2544-AlternatingDigitSum.java
@@ -1,17 +1,9 @@
-// Last updated: 13/07/2026, 11:17:42
+// Last updated: 13/07/2026, 11:22:17
 1class Solution {
-2    public int alternateDigitSum(int n) {
-3        String s=String.valueOf(n);
-4        int sum=0;
-5        for(int i=0;i<s.length();i++){
-6            int digit=s.charAt(i)-'0';
-7            if(i%2==0){
-8                sum+=digit;
-9            }
-10            else{
-11                sum-=digit;
-12            }
-13        }
-14        return sum;
-15    }
-16}
+2    public int distinctIntegers(int n) {
+3        if(n==1){
+4            return 1;
+5        }
+6        return n-1;
+7    }
+8}
