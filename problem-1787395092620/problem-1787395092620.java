db06cd901a198d5a879f0c94da259d18da5eb561
Kavya Sree S S <kavyasreesundar9@gmail.com>
2026-08-22 16:08:16 +0530

    Create

---
+++ problem-1787395092620/problem-1787395092620.java
@@ -0,0 +1,10 @@
+// Last updated: 22/08/2026, 16:08:12
+1class Solution {
+2    public int countVowelStrings(int n) {
+3        int ans = 1;
+4        for (int i = 1; i <= 4; i++) {
+5            ans = ans * (n + i) / i;
+6        }
+7        return ans;
+8    }
+9}
