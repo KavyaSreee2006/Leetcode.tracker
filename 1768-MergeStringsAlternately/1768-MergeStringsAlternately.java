b6c660a28cd8fbba8c0f5eb86ec578dcb0c110f7
Kavya Sree S S <kavyasreesundar9@gmail.com>
2026-08-22 12:04:15 +0530

    File updated at 22/08/2026, 12:04:10

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
@@ -1,16 +1,17 @@
-// Last updated: 22/08/2026, 12:00:58
+// Last updated: 22/08/2026, 12:04:10
 1class Solution {
 2    public String mergeAlternately(String word1, String word2) {
 3        String res="";
-4        int max=Math.max(word1.length(),word2.length());
-5        for(int i=0;i<max;i++){
+4        int i=0;
+5        while(i<word1.length() ||i<word2.length()){
 6            if(i<word1.length()){
 7                res+=word1.charAt(i);
 8            }
 9            if(i<word2.length()){
 10                res+=word2.charAt(i);
 11            }
-12        }
-13        return res;
-14    }
-15}
+12            i++;
+13        }
+14        return res;
+15    }
+16}
