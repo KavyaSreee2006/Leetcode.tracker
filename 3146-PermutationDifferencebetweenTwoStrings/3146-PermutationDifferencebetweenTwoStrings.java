bc627d663ad03c1119712a04f1efa1c18eff58b8
Kavya Sree S S <kavyasreesundar9@gmail.com>
2026-09-01 13:57:46 +0530

    File updated at 01/09/2026, 13:57:45

--- 3146-PermutationDifferencebetweenTwoStrings/3146-PermutationDifferencebetweenTwoStrings.java
+++ 3146-PermutationDifferencebetweenTwoStrings/3146-PermutationDifferencebetweenTwoStrings.java
@@ -1,17 +1,15 @@
-// Last updated: 01/09/2026, 13:57:27
+// Last updated: 01/09/2026, 13:57:45
 1class Solution {
 2    public int findPermutationDifference(String s, String t) {
 3        int ans = 0;
-4
-5        for (int i = 0; i < s.length(); i++) {
-6            for (int j = 0; j < t.length(); j++) {
-7                if (s.charAt(i) == t.charAt(j)) {
-8                    ans += Math.abs(i - j);
-9                    break;
-10                }
-11            }
-12        }
-13
-14        return ans;
-15    }
-16}
+4        for (int i = 0; i < s.length(); i++) {
+5            for (int j = 0; j < t.length(); j++) {
+6                if (s.charAt(i) == t.charAt(j)) {
+7                    ans += Math.abs(i - j);
+8                    break;
+9                }
+10            }
+11        }
+12        return ans;
+13    }
+14}
