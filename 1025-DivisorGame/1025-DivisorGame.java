ac562bbd98bceac7b7f0ace0dae8c1cd870282d0
Kavya Sree S S <kavyasreesundar9@gmail.com>
2026-09-07 14:28:42 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/1025-DivisorGame.java
@@ -0,0 +1,10 @@
+// Last updated: 07/09/2026, 14:28:42
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0)
+        return true;
+        else
+        return false;
+        
+    }
+}
